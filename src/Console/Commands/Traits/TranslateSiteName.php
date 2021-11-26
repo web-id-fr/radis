a541f7daf20659de3a24501aec73453b4f8b8d29
tentacode <contact@gabrielpillet.com>
2021-11-26 16:03:32 +0100

    :bug: Fixing bug on Laravel Forge if site name is too long

--- src/Console/Commands/Traits/TranslateSiteName.php
+++ src/Console/Commands/Traits/TranslateSiteName.php
@@ -11,6 +11,12 @@ trait TranslateSiteName
     {
         $siteName = preg_replace("/[^\w_]+/i", '-', $siteName);
 
+        // limiting the site name or else there is some 
+        // validation errors in Laravel Forge
+        $siteName = substr($siteName, 0, 20);
+
+        $siteName = trim($siteName, '-');
+
         return $siteName;
     }
 }
